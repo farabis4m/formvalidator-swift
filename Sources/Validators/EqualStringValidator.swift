f7832983d547f788dd6ee57e989f557204792136
Shibili KP <shibilikp@gmail.com>
2018-01-10 12:19:10 +0400

    KSA-417- Equal String Validation

---
+++ Sources/Validators/EqualStringValidator.swift
@@ -0,0 +1,47 @@
+//
+//  EqualStringValidator.swift
+//  FormValidatorSwift iOS
+//
+//  Created by Muhammed Shibili on 1/10/18.
+//  Copyright © 2018 ustwo. All rights reserved.
+//
+
+import Foundation
+
+/**
+ *  The `MinimumValueValidator` contains an `PresentCondition`. A valid string is a non-empty string and also have some minimum value
+ *
+ */
+public struct EqualStringValidator: Validator {
+    
+    // MARK: - Properties
+    
+    public var conditions: [Condition]
+
+    // MARK: - Initializers
+    
+    public init() {
+        conditions = [SameCondition()]
+    }
+    
+    public init(comparisonString: String?, errorCode: String) {
+        conditions = [SameCondition(comparisonString: comparisonString, errorCode: errorCode)]
+    }
+    
+    public static func checkValue(ioValue: AnyObject?, comparisonString:String?, errorCode:String) throws -> Bool {
+        
+        let validator = EqualStringValidator(comparisonString: comparisonString, errorCode: errorCode)
+        let conditions = validator.checkConditions(ioValue)
+        //         validator.errorCode = errorCode
+        
+        guard conditions == nil else {
+            if let error =  ErrorMessageProvider.sharedInstance.errorWithCode(errorCode) {
+                throw error
+            }
+            return false
+        }
+        
+        return true
+    }
+    
+}
